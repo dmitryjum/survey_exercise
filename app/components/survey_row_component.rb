c7432ca1bf13d08c6453b8a1b57e4ca6018932be
Dmitry Jum <xfiler@ymail.com>
2025-06-23 07:13:19 -0700

    WIP: answers the survey and returns the status, but incorrectly replaces a survey row

--- app/components/survey_row_component.rb
+++ app/components/survey_row_component.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class SurveyRowComponent < ViewComponent::Base
+  with_collection_parameter :survey
   def initialize(survey:)
     @survey = survey
   end
--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,7 +1,7 @@
 class ResponsesController < ApplicationController
   def create
     @survey = Survey.find(params[:survey_id])
-    @response = @survey.responses.create!(answer: params[:answer] == "yes")
+    @response = @survey.responses.create!(answer: params[:answer])
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_to root_path }
